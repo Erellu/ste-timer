9adbed9166a5fd909731b646febef2aa13997ed6
Erellu <71142033+Erellu@users.noreply.github.com>
2021-04-28 22:17:47 +0200

    LICENSE MODIFICATION: ste-Timer is now released under BSD-2

--- include/timer.hpp
+++ include/timer.hpp
@@ -10,28 +10,37 @@
         • Single shot or loop execution editable at runtime.
         • Function change while running (provided the two decltype()s are identical).
 
-    @copyright     Copyright (C) <2020-2021>  <DUHAMEL Erwan>
+     @copyright     Copyright (C) <2020-2021>  DUHAMEL Erwan
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+                        BSD-2 License
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    All rights reserved.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    Redistribution and use in source and binary forms, with or without modification,
+    are permitted provided that the following conditions are met:
+
+        * Redistributions of source code must retain the above copyright notice,
+          this list of conditions and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright notice,
+          this list of conditions and the following disclaimer in the documentation
+          and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+    A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+    OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+    PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
      @author DUHAMEL Erwan (erwanduhamel@outlook.com)
 */
 
-
-
 /**
-
 EXAMPLE :
 
 
@@ -120,11 +129,11 @@ public:
           const bool &single_shot = true
          )
         :
-          _stopped(true),             //ste::Timer is stopped when declared.
+          _stopped(true),               //ste::Timer is stopped when declared.
           _is_single_shot(single_shot),
           _ms_delay(delay),
           _function(function)
-    {};
+    {}
 
     /**
         @brief Alternate constructor. Avoids having to cast delay into std::chrono:milliseconds
